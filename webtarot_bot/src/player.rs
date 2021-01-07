8cbdd60386f6d5772fbf1627d0023ab5914a0b37
Henri Bourcereau <hbourcereau@atixnet.fr>
2021-01-07 21:50:43 +0100

    tarot_bot : exchange tarot messages and commands in websocket

--- webtarot_bot/src/player.rs
+++ webtarot_bot/src/player.rs
@@ -1,7 +1,13 @@
-use tungstenite::{connect, Message};
+use tungstenite::{connect, Message as TMessage};
+use tungstenite::stream::Stream;
+use tungstenite::protocol::WebSocket;
+
 use url::Url;
+use serde_json::Result;
 
 use tarotgame::{deal_seeded_hands, cards::Hand} ;
+use webtarot_protocol::{Message, Command};
+use webgame_protocol::{AuthenticateCommand, PlayerInfo};
 
 pub fn play() {
     env_logger::init();
@@ -9,17 +15,57 @@ pub fn play() {
     let (mut socket, response) =
         connect(Url::parse("ws://127.0.0.1:8001/ws/new_new").unwrap()).expect("Can't connect");
 
-    println!("Connected to the server");
-    // println!("Response HTTP code: {}", response.status());
-    // println!("Response contains the following headers:");
-    // for (ref header, _value) in response.headers() {
-    //     println!("* {}", header);
-    // }
-
-    socket.write_message(Message::Text(r#"{"cmd": "show_uuid"}"#.into())).unwrap();
-    loop {
-        let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
-    }
-    socket.close(None);
+    let mut player1 = SocketPlayer { socket };
+    player1.play();
 }
+
+struct SocketPlayer {
+    socket: WebSocket<Stream<std::net::TcpStream, native_tls::TlsStream<std::net::TcpStream>>>,
+}
+
+impl Drop for SocketPlayer {
+    fn drop(&mut self) {
+        self.socket.close(None);
+    }
+}
+
+impl SocketPlayer {
+    pub fn play(&mut self){
+        self.send(&Command::Authenticate(AuthenticateCommand { nickname: String::from("Henri") }));
+
+        
+        loop {
+            let msg = self.socket.read_message().expect("Error reading message");
+            let msg = match msg {
+                tungstenite::Message::Text(s) => { s }
+                _ => { panic!() }
+            };
+            let message: Message = serde_json::from_str(&msg).expect("Can't parse JSON");
+            self.handle_server_message(message);
+        }
+
+    }
+
+    fn send(&mut self, command: &Command) -> Result<()> {
+        let json = serde_json::to_string(command)?;
+        self.socket.write_message(TMessage::Text(json)).unwrap();
+        Ok(())
+    }
+
+    fn handle_server_message(&mut self, msg: Message){
+        match msg {
+            Message::Authenticated(PlayerInfo { id, nickname }) => {
+                println!("Authenticated with id {}", id);
+                self.send(&Command::Ping);
+            }
+            Message::Pong => {
+                println!("Received a pong !!");
+            }
+            _ => {
+                println!("Unmanaged server message: {:?}", msg);
+            }
+        }
+    }
+
+}
+

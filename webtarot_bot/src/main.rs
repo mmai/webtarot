a78a5f3e58b3cdfd7a5372a5bbe46b2315ff9e0c
Henri Bourcereau <hbourcereau@atixnet.fr>
2021-01-08 21:35:42 +0100

    bot : connect to game and pass

--- webtarot_bot/src/main.rs
+++ webtarot_bot/src/main.rs
@@ -24,14 +24,21 @@ pub fn main() {
              .value_name("COMMAND")
              .help("Command to execute")
              .takes_value(true))
+        .arg(Arg::with_name("joincode")
+             .short("j")
+             .long("join_code")
+             .value_name("JOINCODE")
+             .help("Game join code")
+             .takes_value(true))
         ;
     let matches = app.get_matches();
 
     let str_command = matches.value_of("command").unwrap_or("play"); 
+    let str_joincode = matches.value_of("joincode").unwrap_or("xxx"); 
 
     match str_command {
         "find_decks" => explorer::find_decks(),
-        "play" => player::play(),
+        "play" => player::play(str_joincode.to_string()),
         _ => println!("Nothing to do")
     }
 }
--- webtarot_bot/src/player.rs
+++ webtarot_bot/src/player.rs
@@ -2,25 +2,29 @@ use tungstenite::{connect, Message as TMessage};
 use tungstenite::stream::Stream;
 use tungstenite::protocol::WebSocket;
 
+use uuid::Uuid;
 use url::Url;
 use serde_json::Result;
 
 use tarotgame::{deal_seeded_hands, cards::Hand} ;
-use webtarot_protocol::{Message, Command};
-use webgame_protocol::{AuthenticateCommand, PlayerInfo};
+use webtarot_protocol::{Message, Command, GameStateSnapshot, PlayerAction, GamePlayCommand, GamePlayerState};
+use webgame_protocol::{AuthenticateCommand, JoinGameCommand, PlayerInfo};
 
-pub fn play() {
+pub fn play(join_code: String) {
     env_logger::init();
 
     let (mut socket, response) =
         connect(Url::parse("ws://127.0.0.1:8001/ws/new_new").unwrap()).expect("Can't connect");
 
-    let mut player1 = SocketPlayer { socket };
+    let mut player1 = SocketPlayer { socket, join_code, game_state: GameStateSnapshot::default(), player_info: PlayerInfo { id: Uuid::default(), nickname: "nobody".into()  }  };
     player1.play();
 }
 
 struct SocketPlayer {
     socket: WebSocket<Stream<std::net::TcpStream, native_tls::TlsStream<std::net::TcpStream>>>,
+    join_code: String,
+    game_state: GameStateSnapshot,
+    player_info: PlayerInfo,
 }
 
 impl Drop for SocketPlayer {
@@ -32,8 +36,6 @@ impl Drop for SocketPlayer {
 impl SocketPlayer {
     pub fn play(&mut self){
         self.send(&Command::Authenticate(AuthenticateCommand { nickname: String::from("Henri") }));
-
-        
         loop {
             let msg = self.socket.read_message().expect("Error reading message");
             let msg = match msg {
@@ -46,6 +48,14 @@ impl SocketPlayer {
 
     }
 
+    pub fn my_state(&self) -> &GamePlayerState {
+        self.game_state
+            .players
+            .iter()
+            .find(|state| state.player.id == self.player_info.id)
+            .unwrap()
+    }
+
     fn send(&mut self, command: &Command) -> Result<()> {
         let json = serde_json::to_string(command)?;
         self.socket.write_message(TMessage::Text(json)).unwrap();
@@ -54,16 +64,41 @@ impl SocketPlayer {
 
     fn handle_server_message(&mut self, msg: Message){
         match msg {
-            Message::Authenticated(PlayerInfo { id, nickname }) => {
-                println!("Authenticated with id {}", id);
-                self.send(&Command::Ping);
+            Message::Authenticated(player_info) => {
+                self.player_info = player_info;
+                println!("Authenticated with id {}", self.player_info.id);
+                self.send(&Command::JoinGame(JoinGameCommand { join_code: self.join_code.clone(), }));
             }
+            Message::GameJoined(game_info) => {
+                println!("Game joined: {}", game_info.game_id);
+                self.send(&Command::MarkReady);
+            }
+            Message::GameStateSnapshot(game_state) => {
+                self.game_state = game_state;
+                self.handle_new_state();
+            }
+            Message::Chat(_) => {}
             Message::Pong => {
                 println!("Received a pong !!");
             }
             _ => {
                 println!("Unmanaged server message: {:?}", msg);
             }
+        }
+    }
+
+    fn handle_new_state(&mut self){
+        let my_state = self.my_state();
+        // let card_played = self.game_state.deal.last_trick.card_played(my_state.pos);
+        let player_action = my_state.get_turn_player_action(self.game_state.turn);
+        // let mypos = my_state.pos.to_n();
+        // let is_my_turn = self.game_state.get_playing_pos() == Some(self.my_state().pos);
+        match player_action {
+            Some(PlayerAction::Bid) => {
+                println!("I pass...");
+                self.send(&Command::GamePlay(GamePlayCommand::Pass));
+            }
+            _ => {}
         }
     }
 

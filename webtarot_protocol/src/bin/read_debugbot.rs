4727e8190f4fffe51526f807207f7f4f74691596
Henri Bourcereau <henri.bourcereau@gmail.com>
2026-03-19 11:33:36 +0100

    fix: save petit when partner plays 21 (and the excuse has not been played) #45

--- webtarot_protocol/src/bin/read_debugbot.rs
+++ webtarot_protocol/src/bin/read_debugbot.rs
@@ -7,8 +7,8 @@ fn main() {
     let mut i = 1;
     while i < args.len() {
         let file_path = args[i].as_str();
-        println!("Reading {file_path}...");
-        let json_str = fs::read_to_string(file_path).expect("Could not read file");
+        let error = format!("Could not read file {file_path}...");
+        let json_str = fs::read_to_string(file_path).expect(&error);
         let game: TarotGameState = serde_json::from_str(&json_str).expect("Error parsing JSON");
         println!("{game}");
         i += 1;
